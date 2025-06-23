6bbfb80df944af0613cc7f68c826f6261156382a
HCaupert <hcaupert@takima.fr>
2025-06-23 17:58:55 +0200

    removed late stuff

--- gateway/src/main/kotlin/fr/hcaupert/gateway/SimpleTemporalStarter.kt
+++ gateway/src/main/kotlin/fr/hcaupert/gateway/SimpleTemporalStarter.kt
@@ -3,7 +3,6 @@ package fr.hcaupert.gateway
 import fr.hcaupert.inventoryserviceapi.InventoryService
 import fr.hcaupert.shippingserviceapi.ShippingService
 import io.temporal.client.WorkflowClient
-import io.temporal.client.WorkflowOptions
 import io.temporal.worker.WorkerFactory
 import jakarta.annotation.PostConstruct
 import org.springframework.stereotype.Service
@@ -17,22 +16,9 @@ class SimpleTemporalStarter(
     private val inventoryService: InventoryService,
 ) {
 
-    private val worker = workerFactory.newWorker("order-queue")
-        .apply {
-            registerWorkflowImplementationTypes(SimpleOrderWorkflowImpl::class.java)
-            registerActivitiesImplementations(inventoryService, shippingService)
-        }
-
     @PostConstruct()
     fun runWorkflowOnStartup() {
         val order = randomCreator.order()
 
-        val options = WorkflowOptions {
-            setWorkflowId("order-" + order.id)
-            setTaskQueue("order-queue")
-        }
-        val processOrderWorkflow = workflowClient.newWorkflowStub(SimpleOrderWorkflow::class.java, options)
-
-        WorkflowClient.start(processOrderWorkflow::processOrder, order)
     }
 }
